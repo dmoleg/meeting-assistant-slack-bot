0148e39291a9bc6cb8624adf9fe49542c309b5d7
Oleg Sviderskij <oleg@sviderskij.lt>
2019-04-03 11:21:29 +0300

    Simple meeting booking flow

--- src/main/java/lt/bit/webservices/controller/R2D2Bot.java
+++ src/main/java/lt/bit/webservices/controller/R2D2Bot.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.socket.WebSocketSession;
 
 import javax.validation.Valid;
+import java.util.regex.Matcher;
 
 @Component
 public class R2D2Bot extends Bot {
@@ -30,5 +31,39 @@ public class R2D2Bot extends Bot {
     @Controller(events = {EventType.DIRECT_MENTION, EventType.DIRECT_MESSAGE})
     public void onReceiveDM(WebSocketSession session, Event event) {
         reply(session, event, new Message("Hi, I am " + slackService.getCurrentUser().getName()));
+    }
+
+    @Controller(events = {EventType.DIRECT_MESSAGE, EventType.MESSAGE}, pattern = "(setup meeting)", next = "confirmTiming")
+    public void setupMeeting(WebSocketSession session, Event event) {
+        startConversation(event, "confirmTiming");
+        reply(session, event, "Cool! At what time (ex. 15:30) do you want me to set up the meeting?");
+    }
+
+    @Controller(next = "askTimeForMeeting")
+    public void confirmTiming(WebSocketSession session, Event event) {
+        reply(session, event, "Your meeting is set at " + event.getText() +
+                ". Would you like to repeat it tomorrow?");
+        nextConversation(event);
+    }
+
+    @Controller(next = "askWhetherToRepeat")
+    public void askTimeForMeeting(WebSocketSession session, Event event) {
+        if (event.getText().contains("yes")) {
+            reply(session, event, "Okay. Would you like me to set a reminder for you?");
+            nextConversation(event);
+        } else {
+            reply(session, event, "No problem. You can always schedule one with 'setup meeting' command.");
+            stopConversation(event);
+        }
+    }
+
+    @Controller
+    public void askWhetherToRepeat(WebSocketSession session, Event event) {
+        if (event.getText().contains("yes")) {
+            reply(session, event, "Great! I will remind you tomorrow before the meeting.");
+        } else {
+            reply(session, event, "Okay, don't forget to attend the meeting tomorrow :)");
+        }
+        stopConversation(event);
     }
 }
